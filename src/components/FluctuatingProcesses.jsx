a6d6cd58d09a3cfd04018f77681c98ed9830f463
leew1e <ostrovdmytriy@gmail.com>
2024-09-07 10:05:25 +0300

    Apply correct logic

--- src/components/FluctuatingProcesses.jsx
+++ src/components/FluctuatingProcesses.jsx
@@ -10,41 +10,52 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const SMOOTH_MULTIPLIER = 30;
+// Calculate using next starting values setting from control panel:
+// x0 - initial position (m)
+// v0 - initial velocity (m/s)
+// m - mass (kg)
+// c - spring constant (N/m)
+// u - phase (rad)
+// T - period (s)
+
+// Need to calculate:
+// k - frequency (Hz)
+// x - current position (from t) (m)
 
 const FluctuatingProcesses = () => {
-  const [amplitude, setAmplitude] = useState(1);
-  const [frequency, setFrequency] = useState(1);
-  const [phase, setPhase] = useState(0);
-  const [damping, setDamping] = useState(0);
+  const [x0, setX0] = useState(1); // initial position (m)
+  const [v0, setV0] = useState(0); // initial velocity (m/s)
+  const [m, setM] = useState(1); // mass (kg)
+  const [c, setC] = useState(1); // spring constant (N/m)
+  const [u, setU] = useState(0); // phase (rad)
+  const [T, setT] = useState(1); // period (s)
   const [isSimulating, setIsSimulating] = useState(false);
   const [time, setTime] = useState(0);
   const [data, setData] = useState([]);
+  const [smoothMultiplier, setSmoothMultiplier] = useState(30);
 
   useEffect(() => {
     if (isSimulating) {
       const interval = setInterval(() => {
         setTime((prevTime) => {
-          const newTime = prevTime + 1 / SMOOTH_MULTIPLIER;
-          const omega = 2 * Math.PI * frequency;
+          const newTime = prevTime + 1 / smoothMultiplier;
+          const k = 1 / T; // frequency (Hz)
+          const omega = 2 * Math.PI * k;
           const x =
-            amplitude *
-            Math.exp(-damping * newTime) *
-            Math.sin(omega * newTime + phase);
+            x0 * Math.cos(omega * newTime + u) +
+            (v0 / omega) * Math.sin(omega * newTime + u);
           const v =
-            amplitude *
-            Math.exp(-damping * newTime) *
-            (omega * Math.cos(omega * newTime + phase) -
-              damping * Math.sin(omega * newTime + phase));
+            -x0 * omega * Math.sin(omega * newTime + u) +
+            v0 * Math.cos(omega * newTime + u);
 
           setData((prevData) => [...prevData, { t: newTime, x, v }]);
           return newTime;
         });
-      }, 1000 / SMOOTH_MULTIPLIER);
+      }, 1000 / smoothMultiplier);
 
       return () => clearInterval(interval);
     }
-  }, [isSimulating, amplitude, frequency, phase, damping]);
+  }, [isSimulating, x0, v0, m, c, u, T, smoothMultiplier]);
 
   const handleSimulation = () => {
     setIsSimulating(!isSimulating);
@@ -58,9 +69,9 @@ const FluctuatingProcesses = () => {
 
   const renderOscillationSimulation = () => {
     const position = data.length > 0 ? data[data.length - 1].x : 0;
-    const maxAmplitude = amplitude * 2;
+    const maxAmplitude = x0 * 2;
     const circleSize = 16; // 4rem = 16px
-    const circlePosition = ((position + amplitude) / maxAmplitude) * 100;
+    const circlePosition = ((position + x0) / maxAmplitude) * 100;
     const clampedPosition = Math.max(0, Math.min(100, circlePosition));
 
     return (
@@ -69,14 +80,14 @@ const FluctuatingProcesses = () => {
           className="absolute top-0 bottom-0 w-4 h-4 bg-blue-500 rounded-full transform -translate-y-1/2"
           style={{
             left: `calc(${clampedPosition}% - ${circleSize / 2}px)`,
-            top: "50%"
+            top: "50%",
           }}
         ></div>
         <div className="absolute left-0 top-1/2 transform -translate-y-1/2 text-xs text-gray-600 ml-2">
-          {-amplitude.toFixed(2)}
+          {-x0.toFixed(2)}
         </div>
         <div className="absolute right-0 top-1/2 transform -translate-y-1/2 text-xs text-gray-600 mr-2">
-          {amplitude.toFixed(2)}
+          {x0.toFixed(2)}
         </div>
       </div>
     );
@@ -88,7 +99,9 @@ const FluctuatingProcesses = () => {
         return (
           <div className="bg-white p-2 border border-gray-300 rounded shadow">
             <p className="text-sm">{`Time: ${label.toFixed(2)}`}</p>
-            <p className="text-sm">{`${title.split(" ")[0]}: ${payload[0].value.toFixed(2)}`}</p>
+            <p className="text-sm">{`${
+              title.split(" ")[0]
+            }: ${payload[0].value.toFixed(2)}`}</p>
           </div>
         );
       }
@@ -103,11 +116,19 @@ const FluctuatingProcesses = () => {
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis
               dataKey="t"
-              label={{ value: "Time", position: "insideBottomRight", offset: -10 }}
+              label={{
+                value: "Time",
+                position: "insideBottomRight",
+                offset: -10,
+              }}
               tickFormatter={(value) => value.toFixed(2)}
             />
             <YAxis
-              label={{ value: title.split(" ")[0], angle: -90, position: "insideLeft" }}
+              label={{
+                value: title.split(" ")[0],
+                angle: -90,
+                position: "insideLeft",
+              }}
               tickFormatter={(value) => value.toFixed(2)}
             />
             <Tooltip content={<CustomTooltip />} />
@@ -126,7 +147,7 @@ const FluctuatingProcesses = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 p-4 sm:p-6 md:p-8 w-full">
-      <h1 className="text-3xl font-bold mb-6 text-blue-700">Fluctuations</h1>
+      <h1 className="text-3xl font-bold mb-6 text-blue-500">Fluctuations</h1>
       <div className="flex flex-row gap-6">
         {/* Left side: Graphs and Simulation */}
         <div className="w-5/6 space-y-6">
@@ -155,61 +176,96 @@ const FluctuatingProcesses = () => {
               <div className="flex-grow space-y-4">
                 <div>
                   <label className="block text-sm font-medium text-gray-700">
-                    Amplitude
+                    x0 (m)
                   </label>
                   <input
                     type="number"
-                    value={amplitude}
-                    onChange={(e) => setAmplitude(parseFloat(e.target.value))}
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                    value={x0}
+                    onChange={(e) => setX0(parseFloat(e.target.value))}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-1 px-2 bg-gray-50"
                   />
                 </div>
                 <div>
                   <label className="block text-sm font-medium text-gray-700">
-                    Frequency
+                    v0 (m/s)
                   </label>
                   <input
                     type="number"
-                    value={frequency}
-                    onChange={(e) => setFrequency(parseFloat(e.target.value))}
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                    value={v0}
+                    onChange={(e) => setV0(parseFloat(e.target.value))}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-1 px-2 bg-gray-50"
                   />
                 </div>
                 <div>
                   <label className="block text-sm font-medium text-gray-700">
-                    Phase
+                    Mass (kg)
                   </label>
                   <input
                     type="number"
-                    value={phase}
-                    onChange={(e) => setPhase(parseFloat(e.target.value))}
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                    value={m}
+                    onChange={(e) => setM(parseFloat(e.target.value))}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-1 px-2 bg-gray-50"
                   />
                 </div>
                 <div>
                   <label className="block text-sm font-medium text-gray-700">
-                    Damping
+                    c (N/m)
                   </label>
                   <input
                     type="number"
-                    value={damping}
-                    onChange={(e) => setDamping(parseFloat(e.target.value))}
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                    value={c}
+                    onChange={(e) => setC(parseFloat(e.target.value))}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-1 px-2 bg-gray-50"
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    u (rad)
+                  </label>
+                  <input
+                    type="number"
+                    value={u}
+                    onChange={(e) => setU(parseFloat(e.target.value))}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-1 px-2 bg-gray-50"
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    T (s)
+                  </label>
+                  <input
+                    type="number"
+                    value={T}
+                    onChange={(e) => setT(parseFloat(e.target.value))}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-1 px-2 bg-gray-50"
                   />
                 </div>
               </div>
               <div className="space-y-2">
-                <button
-                  onClick={handleSimulation}
-                  className="w-full px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-150 ease-in-out"
-                >
-                  {isSimulating ? "Stop" : "Start"}
-                </button>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Smooth Multiplier
+                  </label>
+                  <input
+                    type="number"
+                    value={smoothMultiplier}
+                    onChange={(e) =>
+                      setSmoothMultiplier(parseInt(e.target.value))
+                    }
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-1 px-2 bg-gray-50"
+                  />
+                </div>
                 <button
                   onClick={handleRestartSimulation}
                   className="w-full px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition duration-150 ease-in-out"
                 >
                   Restart
+                </button>
+                <button
+                  onClick={handleSimulation}
+                  className="w-full px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-150 ease-in-out"
+                >
+                  {isSimulating ? "Stop" : "Start"}
                 </button>
               </div>
             </div>
